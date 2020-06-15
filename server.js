b0c8a3246e87d8fabab881c11e52d7c650029bb8
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-06-14 23:14:50 -0400

    Working on the get API commands to read in the db.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -14,24 +15,41 @@ app.use(express.json());
 
 //Add Code Here
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+// fs.readFileSync("./db/db.json", 'utf8')
 
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+let noteDump = [];
+
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    
+    try{
+    noteDump = fs.readFileSync("./develop/db/db.json", 'utf8');
+    console.log("IT WORKED!")
+    noteDump = JSON.parse(noteDump);
+    }
+
+    catch (err){
+        console.log(err);
+    }
+
+    res.json(noteDump);
 });
 
 app.post("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "./develop/db/db.json"));
 });
 
 app.delete("/api/notes/:id", function(reg,res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "./develop/db/db.json"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"))
+    
+});
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
 });
 
 // Starts the server to begin listening
